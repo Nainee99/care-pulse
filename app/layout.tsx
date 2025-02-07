a82f4a367fa46731e2fbab1b5c8383b10b445d92
Hasnain Arif <hasnainarif571@gmail.com>
2025-02-07 11:48:39 +0500

    remove unused images and add new UI components for form handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
-import { ThemeProvider } from "next-theme";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 import { cn } from "@/lib/utils";
 
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen bg-dark-300 font-sans antialiased",
